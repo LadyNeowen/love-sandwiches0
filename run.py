89609cc0c28400a001dd8dd28bf02f1a721e74db
LadyNeowen <Alicia.Westerlund@outlook.com>
2024-11-06 11:35:37 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -49,7 +49,7 @@ def validate_data(values):
                 f'Exactly 6 values required, you provided {len(values)}'
             )
     except ValueError as e:
-        print(f'Invalid data: {e}, please try again.\n')
+        print(f'Invalid data: {e}, Please try again.\n')
         return False
 
     return True
@@ -103,6 +103,21 @@ def get_last_5_entries_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -113,9 +128,11 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
+    sales_columns = get_last_5_entries_sales()
+    calculate_stock_data(sales_columns)
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 
 print('Welcome to Love Sandwiches Data Automation')
-#main()
-
-sales_columns = get_last_5_entries_sales()
+main()
